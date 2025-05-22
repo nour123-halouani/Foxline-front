b4b8524ec6a6220dda978dcdae49488d3f6da296
Nour Halouani <nourhalouani2@gmail.com>
2025-05-22 21:33:40 +0100

    redux setup

--- src/app/(warehouse)/loading.tsx
+++ src/app/(warehouse)/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from "rizzui";
 export default function Loading() {
     return (
         <div className="flex items-center justify-center h-[80vh]">
-            <Loader color="primary" />
+            <Loader color="primary" size="xl"/>
         </div>
     );
 }
