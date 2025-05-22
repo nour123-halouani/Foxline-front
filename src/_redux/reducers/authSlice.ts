f33d1bb4ce88decbd19dfcbe844e04492e5e77d3
Nour Halouani <nourhalouani2@gmail.com>
2025-05-22 22:18:28 +0100

    redux setup

--- src/_redux/reducers/authSlice.ts
+++ src/_redux/reducers/authSlice.ts
@@ -1,10 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { checkAccessToken } from "@/_redux/actions/auth";
-
-export const initializeAuth = createAsyncThunk("auth/checkAuth", async () => {
-  await checkAccessToken();
-  return true;
-});
+import { createSlice } from "@reduxjs/toolkit";
 
 interface User {
   id: string;
@@ -15,11 +9,6 @@ interface User {
   role: string;
 }
 
-interface Settings {
-  shopDomain: string;
-  apiKey: string;
-}
-
 const initialState: any = {
   settings: null,
   error: null,
@@ -27,48 +16,11 @@ const initialState: any = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {
-    loginSuccess(state, action: PayloadAction<User>) {
-      state.isAuthenticated = true;
-      state.user = action.payload;
-      state.error = null;
-    },
-    loginFailure(state, action: PayloadAction<string>) {
-      state.isAuthenticated = false;
-      state.user = null;
-      state.error = action.payload;
-    },
-    logoutAction(state) {
-      state.isAuthenticated = false;
-      state.user = null;
-      state.error = null;
-      console.log('state.isAuthenticated', state.isAuthenticated)
-    },
-    updatedSuccess(state, action: PayloadAction<Settings>) {
-      state.settings = action.payload;
-      state.error = null;
-    },
-    updatedFailure(state, action: PayloadAction<string>) {
-      state.settings = null;
-      state.error = action.payload;
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(initializeAuth.fulfilled, (state, action) => {
-      state.isAuthenticated = true;
-    });
-    builder.addCase(initializeAuth.rejected, (state) => {
-      state.isAuthenticated = false;
-    });
   },
 });
 
-export const {
-  loginSuccess,
-  loginFailure,
-  logoutAction,
-  updatedFailure,
-  updatedSuccess,
-} = authSlice.actions;
+export const { } = authSlice.actions;
 
 export default authSlice.reducer;
